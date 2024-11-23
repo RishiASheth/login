ddfcefab528f54b2d79037595fa2bc0db56af517
Rishi A Sheth <rishisheth404@gmail.com>
2024-11-23 13:10:10 +0530

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb+srv://rishisheth19:8dn2PwXnUrWqqaMU@cluster0.ae8uz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("DATABASE_URL")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
